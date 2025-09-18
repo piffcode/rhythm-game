97cc048c785ce1c749d2a365733dbbf7d40f307b
piffcode <piffcode@gmail.com>
2025-09-18 10:58:13 -0700

    Delete scripts directory

--- scripts/client-config.js
+++
@@ -1,4 +0,0 @@
-// Injected Spotify client ID for the Rhythm Game frontend
-// This file defines the client identifier before config.js runs so that
-// the configuration loader can pick it up from window.__SPOTIFY_CLIENT_ID__.
-window.__SPOTIFY_CLIENT_ID__ = '07f4566e6a2a4428ac68ec86d73adf34';
